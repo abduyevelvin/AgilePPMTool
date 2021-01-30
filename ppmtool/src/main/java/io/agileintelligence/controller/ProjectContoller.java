56119d9deaa7a97abbdf1e4be58efaa549b363f5
Abduyev Elvin <e.abduyev@reply.de>
2021-01-30 13:23:03 +0100

    Refactoring controller

--- ppmtool/src/main/java/io/agileintelligence/controller/ProjectContoller.java
+++ ppmtool/src/main/java/io/agileintelligence/controller/ProjectContoller.java
@@ -2,16 +2,20 @@ package io.agileintelligence.controller;
 
 import io.agileintelligence.model.Project;
 import io.agileintelligence.service.IProjectService;
+import io.agileintelligence.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -26,9 +30,12 @@ public class ProjectContoller {
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-        if (result.hasErrors()) {
-            return new ResponseEntity<>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+
+        ResponseEntity<?> errorMap = Util.mapValidationMethod(result);
+        if (errorMap != null) {
+            return errorMap;
         }
+
         Project created = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
