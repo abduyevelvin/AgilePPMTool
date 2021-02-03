5c2d2c31d4eeb8c5d5bec40b6a5e225c7a212336
Abduyev Elvin <e.abduyev@reply.de>
2021-02-03 14:40:47 +0100

    get method added to react

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,15 +1,22 @@
 import axios from "axios";
-import errorReducer from "../reducers/errorReducer";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
 
 export const createProject = (project, history) => async (dispatch) => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
-  } catch (error) {
+  } catch (err) {
     dispatch({
       type: GET_ERRORS,
-      payload: error.response.data,
+      payload: err.response.data,
     });
   }
+};
+
+export const getProjects = () => async (dispatch) => {
+  const res = await axios.get("http://localhost:8080/api/project");
+  dispatch({
+    type: GET_PROJECTS,
+    payload: res.data,
+  });
 };
